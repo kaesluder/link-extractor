92afc72735c9be8a8f5fc9570285ebc5d404d9ec
Kae Sluder <kaesluder@fastmail.com>
2024-06-15 00:44:28 -0400

    unit tests and documentation for main loading/parsing functions
    Add unit tests for file loading and parsing. Unit tests require test
    markdown files in test_markdown. Also add docstrings for loading
    and parsing functions.

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,16 @@ struct Args {
     separator: String,
 }
 
+/// Load text as `String` from filename
+///
+/// # Inputs
+///
+/// - `filename`: `std::path::PathBuf` pointing to markdown file
+///
+/// # Results
+///
+/// - `Ok(string)`: String contents of file.
+/// - `Err(e)`: Error condition.
 fn load_file(filename: &std::path::PathBuf) -> Result<String, std::io::Error> {
     let mut file = File::open(filename)?;
     let mut contents = String::new();
@@ -33,9 +43,20 @@ fn load_file(filename: &std::path::PathBuf) -> Result<String, std::io::Error> {
     Ok(contents)
 }
 
+/// Parse markdown from `filename` returning a `Result` with a `Vec` of `Link`s
+///
+/// # Inputs
+///
+/// * `filename`: std::path::PathBuf pointing to the markdown file
+///
+/// # Results
+///
+/// - `Ok(links)`: `Vec` of `Link` structs. List may be empty if no links in file.
+/// - `Err(e)`: Error result. Handle this!
+///
 fn parse_from_filename(filename: &std::path::PathBuf) -> Result<Vec<parser::Link>, std::io::Error> {
     let contents = load_file(filename)?;
-    let links = extract_links(&contents, &filename.to_string_lossy().into_owned());
+    let links = extract_links(&contents, &filename.to_string_lossy());
     Ok(links)
 }
 
@@ -69,4 +90,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    // Import everything from the outer module to make it available for tests
+    use super::*;
+
+    /// Check that loading function can load a string
+    #[test]
+    fn load_existing_file_returns_string() -> std::io::Result<()> {
+        let filepath = std::path::PathBuf::from("test_markdown/three_links.md");
+        let test_text = load_file(&filepath)?;
+        assert!(test_text.contains("https://example.com"));
+
+        Ok(())
+    }
+
+    /// Check that loading function returns an err if given a bad filepath
+    #[test]
+    fn load_non_existing_file_returns_err() {
+        let filepath = std::path::PathBuf::from("");
+        assert!(load_file(&filepath).is_err());
+    }
+
+    /// Test parsing of a simple test file in the test_markdown dir
+    /// test file should have three links in the format:
+    /// `[three links: <a, b, c>](https://example.com)`
+    #[test]
+    fn parse_markdown_with_links() {
+        let filepath = std::path::PathBuf::from("test_markdown/three_links.md");
+        let links = parse_from_filename(&filepath).unwrap();
+        assert_eq!(links.len(), 3);
+        assert_eq!(links[0].url, "https://example.com");
+        assert!(links[0].description.contains("three links: a"))
+    }
+
+    /// Test parsing of file without any links
+    /// File can be any format as long as there are no links.
+    /// Parser should return empty list.
+    #[test]
+    fn parse_markdown_without_links() {
+        let filepath = std::path::PathBuf::from("test_markdown/no_links.md");
+        let links = parse_from_filename(&filepath).unwrap();
+        assert_eq!(links.len(), 0)
+    }
+
+    /// Test handling of bad filepath.
+    /// Errors should trickle up to interface logic for handling.
+    #[test]
+    fn parse_nofile_returns_err() {
+        let filepath = std::path::PathBuf::from("");
+        assert!(parse_from_filename(&filepath).is_err());
+    }
 }
